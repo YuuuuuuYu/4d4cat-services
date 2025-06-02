c077257b521bd42cdaf9fceef5856e70093b10d4
Nakji <yuitfhk@gmail.com>
2025-06-02 18:06:44 +0900

    refactor: 데이터 초기화 서비스 추상화 수준 향상

--- src/main/java/com/services/common/application/DataInitializationService.java
+++ src/main/java/com/services/common/application/DataInitializationService.java
@@ -1,27 +1,59 @@
 package com.services.common.application;
 
-import com.services.common.domain.ParameterBuilder;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.services.common.application.dto.ParameterBuilder;
+import com.services.common.exception.BadRequestException;
+import com.services.common.exception.ErrorCode;
+import com.services.common.infrastructure.DataStorage;
+import com.services.common.presentation.dto.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
-public abstract class DataInitializationService {
+public abstract class DataInitializationService<T, P extends ParameterBuilder, R extends ApiResponse<T>> {
 
     protected final RestTemplate restTemplate;
 
-    protected <T, P extends ParameterBuilder> T get(ParameterizedTypeReference<T> responseType, P params) {
+    @EventListener(ApplicationReadyEvent.class)
+    public void initializeData() {
+        List<T> dataList = getFilters().stream()
+                .map(this::fetchDataForFilter)
+                .filter(Objects::nonNull)
+                .flatMap(response -> response.getItems().stream())
+                .toList();
+                
+        DataStorage.setData(getStorageKey(), dataList);
+    }
+
+    protected R fetchDataForFilter(String filter) {
+        try {
+            P params = createParameters(filter);
+            return getApiResponse(getResponseTypeReference(), params);
+        } catch (Exception e) {
+            throw new BadRequestException(ErrorCode.INVALID_REQUEST);
+        }
+    }
+
+    protected R get(ParameterizedTypeReference<R> responseType, P params) {
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(getBaseUrl());
         if (params != null) {
             params.appendToBuilder(builder);
         }
-
-        ResponseEntity<T> response = restTemplate.exchange(
+        
+        ResponseEntity<R> response = restTemplate.exchange(
                 builder.toUriString(),
                 HttpMethod.GET,
                 null,
@@ -30,5 +62,14 @@ public abstract class DataInitializationService {
         return response.getBody();
     }
 
+    protected R getApiResponse(ParameterizedTypeReference<R> responseType, P params) {
+        return get(responseType, params);
+    }
+
     protected abstract String getBaseUrl();
+    protected abstract String getStorageKey();
+    protected abstract List<String> getFilters();
+    protected abstract P createParameters(String filter);
+    protected abstract ParameterizedTypeReference<R> getResponseTypeReference();
+    public abstract void addRandomElementToModel(Model model);
 }
