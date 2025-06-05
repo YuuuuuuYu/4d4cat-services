647ae729289a6559902740b2259edf6d1858e204
Nakji <yuitfhk@gmail.com>
2025-06-05 15:44:45 +0900

    test: 테스트 코드 수정

--- src/test/java/com/services/pixabay/application/PixabayVideoServiceTest.java
+++ src/test/java/com/services/pixabay/application/PixabayVideoServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -57,7 +58,7 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("getBaseUrl Success - 기본 URL 반환")
+    @DisplayName("getBaseUrl - 기본 URL 반환")
     void getBaseUrl_shouldReturnsPixabayVideoUrl() {
         String baseUrl = pixabayVideoService.getBaseUrl();
         
@@ -65,7 +66,7 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("getStorageKey Success - 스토리지 키 반환")
+    @DisplayName("getStorageKey - 스토리지 키 반환")
     void getStorageKey_shouldReturnsPixabayVideoKey() {
         String storageKey = pixabayVideoService.getStorageKey();
         
@@ -73,18 +74,19 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("getFilters Success - 비디오 카테고리 목록 반환")
+    @DisplayName("getFilters - 비디오 카테고리 목록 반환")
     void getFilters_shouldReturnsVideoCategories() {
+        int currentCategorySize = 20;
         List<String> filters = pixabayVideoService.getFilters();
         
         assertAll(
             () -> assertThat(filters).contains("backgrounds", "fashion", "nature", "science", "education"),
-            () -> assertThat(filters).hasSize(20)
+            () -> assertThat(filters).hasSize(currentCategorySize)
         );
     }
 
     @Test
-    @DisplayName("createParameters Success - PixabayVideoRequest 생성")
+    @DisplayName("createParameters - PixabayVideoRequest 생성")
     void createParameters_shouldCreatesPixabayVideoRequest() {
         PixabayVideoRequest request = pixabayVideoService.createParameters("backgrounds");
         
@@ -96,7 +98,26 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("setDataStorage Success - API 호출 시 올바른 URI가 생성되어야 함")
+    @DisplayName("setDataStorage - 데이터 저장할 때 API는 20회 호출")
+    void setDataStorage_shouldCallApiManyTimes_whenInitializingData() {
+        // Given
+        int callApiCount = 20;
+        PixabayTestFixtures.setupRestTemplateToReturnSingleVideo(restTemplate, 1);
+
+        // When
+        pixabayVideoService.setDataStorage();
+
+        // Then
+        verify(restTemplate, times(callApiCount)).exchange(
+            anyString(),
+            eq(HttpMethod.GET),
+            isNull(),
+            any(ParameterizedTypeReference.class)
+        );
+    }
+
+    @Test
+    @DisplayName("setDataStorage - API 호출 시 올바른 URI가 생성되어야 함")
     void setDataStorage_shouldBuildCorrectUri_whenCallingApi() {
         // Given
         PixabayTestFixtures.setupRestTemplateToReturnSingleVideo(restTemplate, 1);
@@ -108,7 +129,7 @@ class PixabayVideoServiceTest {
         ArgumentCaptor<String> urlCaptor = ArgumentCaptor.forClass(String.class);
         List<String> capturedUrls = urlCaptor.getAllValues();
 
-        verify(restTemplate).exchange(
+        verify(restTemplate, atLeastOnce()).exchange(
             urlCaptor.capture(),
             eq(HttpMethod.GET),
             isNull(),
@@ -120,25 +141,7 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("setDataStorage Success - 데이터 저장할 때 API는 20회 호출")
-    void setDataStorage_shouldCallApiTwentyTimes_whenInitializingData() {
-        // Given
-        PixabayTestFixtures.setupRestTemplateToReturnSingleVideo(restTemplate, 1);
-
-        // When
-        pixabayVideoService.setDataStorage();
-
-        // Then
-        verify(restTemplate, times(20)).exchange(
-            anyString(),
-            eq(HttpMethod.GET),
-            isNull(),
-            any(ParameterizedTypeReference.class)
-        );
-    }
-
-    @Test
-    @DisplayName("setDataStorage Success - 데이터 fetch가 완료되면 스토리지에 데이터 저장")
+    @DisplayName("setDataStorage - 데이터 fetch가 완료되면 스토리지에 데이터 저장")
     void setDataStorage_shouldStoreDataInStorage_whenDataFetchCompletes() {
         // Given
         PixabayTestFixtures.setupRestTemplateToReturnSingleVideo(restTemplate, 1);
@@ -152,7 +155,7 @@ class PixabayVideoServiceTest {
         );
 
         // Then
-        verify(restTemplate, times(20)).exchange(
+        verify(restTemplate, atLeastOnce()).exchange(
             anyString(),
             eq(HttpMethod.GET),
             isNull(),
@@ -166,7 +169,7 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("setDataStorage Fail - API 호출이 실패하면 BadRequestException을 던진다")
+    @DisplayName("setDataStorage - API 호출이 실패하면 BadRequestException을 던진다")
     void setDataStorage_shouldThrowBadRequestException_whenApiCallFails() {
         // Given
         when(restTemplate.exchange(
@@ -183,7 +186,7 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("addRandomElementToModel Success - 데이터가 있을 때 모델에 비디오 추가")
+    @DisplayName("addRandomElementToModel - 데이터가 있을 때 모델에 비디오 추가")
     void addRandomElementToModel_shouldAddsVideoToModel_whenDataAvailable() {
         // Given
         PixabayVideoResult videoResult = PixabayTestFixtures.createDefaultVideoResult(1);
@@ -205,7 +208,7 @@ class PixabayVideoServiceTest {
     }
 
     @Test
-    @DisplayName("addRandomElementToModel Fail - 데이터가 없을 경우 모델에 추가하지 않음")
+    @DisplayName("addRandomElementToModel - 데이터가 없을 경우 모델에 추가하지 않음")
     void addRandomElementToModel_shouldNotAddsVideoToModel_whenDataNotFound() {
         // Given
         try (MockedStatic<DataStorage> mockedDataStorage = mockStatic(DataStorage.class)) {
