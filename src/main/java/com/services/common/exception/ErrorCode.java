b276dda908e910441c90ed59e36f439bddf5b95f
Nakji <yuitfhk@gmail.com>
2025-06-05 15:43:20 +0900

    chore: 500 커스텀 에러 추가

--- src/main/java/com/services/common/exception/ErrorCode.java
+++ src/main/java/com/services/common/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     // common errors
     INVALID_REQUEST("CO1000", "Invalid request"),
     DATA_NOT_FOUND("CO1001", "Data not found"),
+    INTERNAL_SERVER_ERROR("CO1002", "Internal server error"),
 
     // pixabay video
     PIXABAY_VIDEO_NOT_FOUND("PV1000", "PixabayVideo data not found"),
