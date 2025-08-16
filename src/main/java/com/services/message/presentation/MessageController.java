0e7034a12847892637f4f7bf2d9a1e7b33ef2225
yuuuuuuyu <yuitfhk@gmail.com>
2025-08-17 04:41:37 +0900

    chore: /message 요청 방식 수정 RequestParam -> RequestBody

--- src/main/java/com/services/message/presentation/MessageController.java
+++ src/main/java/com/services/message/presentation/MessageController.java
@@ -1,17 +1,14 @@
 package com.services.message.presentation;
 
-import com.services.message.application.Message;
 import com.services.message.application.MessageService;
+import com.services.message.presentation.dto.MessageRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +23,8 @@ public class MessageController {
     }
 
     @PostMapping("/message")
-    public ResponseEntity<Void> saveMessage(@RequestParam("content") String content, HttpServletRequest request) {
-        messageService.saveMessage(content, request);
+    public ResponseEntity<Void> saveMessage(@RequestBody MessageRequest body, HttpServletRequest request) {
+        messageService.saveMessage(body, request);
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/services/message/presentation/dto/MessageRequest.java
@@ -0,0 +1,13 @@
+package com.services.message.presentation.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MessageRequest {
+
+    private String content;
+
+    public MessageRequest(String content) {
+        this.content = content;
+    }
+}
