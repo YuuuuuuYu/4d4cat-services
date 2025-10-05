ec71c11ccc21a05f7ff2ddcbadb1fbbc3349dfbd
Nakji <yuitfhk@gmail.com>
2025-10-06 04:56:59 +0900

    chore: implement timestamp-based random generation

--- src/main/java/com/services/common/util/RandomUtils.java
+++ src/main/java/com/services/common/util/RandomUtils.java
@@ -3,6 +3,7 @@ package com.services.common.util;
 public class RandomUtils {
 
     public static int generateRandomInt(int max) {
-        return (int) (Math.random() * (max));
+        long timestamp = System.currentTimeMillis();
+        return (int) (timestamp % max);
     }
 }
