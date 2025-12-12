5fc4b72cee0fb75da2e2efac457f21cf884d0c92
yuuuuuuyu <yuitfhk@gmail.com>
2025-12-12 23:22:49 +0900

    feat: Integrate Discord webhook for initialization alerts

---
+++ src/main/java/com/services/common/infrastructure/discord/DiscordWebhookPayload.java
@@ -0,0 +1,22 @@
+package com.services.common.infrastructure.discord;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DiscordWebhookPayload {
+
+    private String username;
+    private String avatar_url;
+    private String content;
+    private List<Embed> embeds;
+}
