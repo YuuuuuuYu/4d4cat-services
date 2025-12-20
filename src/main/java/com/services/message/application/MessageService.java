88a67d2385552ffd4b30ae9958a469c83a139c1c
yuuuuuuyu <yuitfhk@gmail.com>
2025-12-20 15:34:58 +0900

    chore: Remove unused fields

--- src/main/java/com/services/message/application/MessageService.java
+++ src/main/java/com/services/message/application/MessageService.java
@@ -1,16 +1,16 @@
 package com.services.message.application;
 
+import com.services.common.aop.NotifyDiscord;
 import com.services.common.application.exception.BadRequestException;
 import com.services.common.application.exception.ErrorCode;
 import com.services.message.presentation.dto.MessageRequest;
-import com.services.message.util.WebUtils;
 import com.services.message.validator.MessageValidator;
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -20,17 +20,20 @@ public class MessageService {
 
   private final Map<String, Message> messageStore = new ConcurrentHashMap<>();
 
-  public void saveMessage(MessageRequest body, HttpServletRequest request) {
+  @NotifyDiscord(
+      taskName = "Message Stored",
+      startLog = "Update Message",
+      errorLog = "❌ Failed to update message: %s")
+  public void saveMessage(MessageRequest body) {
     if (MessageValidator.isValid(body.getContent())) {
-      createAndStoreMessage(body.getContent(), request);
+      createAndStoreMessage(body.getContent());
     } else {
       throw new BadRequestException(ErrorCode.MESSAGE_INVALID_REQUEST);
     }
   }
 
-  private void createAndStoreMessage(String content, HttpServletRequest request) {
-    String clientIp = WebUtils.getClientIp(request);
-    Message message = new Message(content, clientIp);
+  private void createAndStoreMessage(String content) {
+    Message message = new Message(content);
     messageStore.put(LAST_MESSAGE, message);
   }
 
