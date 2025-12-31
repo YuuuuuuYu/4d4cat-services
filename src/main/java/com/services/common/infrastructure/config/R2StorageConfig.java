733cc27699024e25d859b274041393b89a2cc5c6
yuuuuuuyu <yuitfhk@gmail.com>
2025-12-31 11:43:16 +0900

    chore: Configure R2 storage

---
+++ src/main/java/com/services/common/infrastructure/config/R2StorageConfig.java
@@ -0,0 +1,33 @@
+package com.services.common.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class R2StorageConfig {
+
+  @Value("${cloudflare.r2.access-key}")
+  private String accessKey;
+
+  @Value("${cloudflare.r2.secret-key}")
+  private String secretKey;
+
+  @Value("${cloudflare.r2.endpoint}")
+  private String endpointUrl;
+
+  @Bean
+  public S3Client s3Client() {
+    return S3Client.builder()
+        .endpointOverride(URI.create(endpointUrl))
+        .credentialsProvider(StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(accessKey, secretKey)
+        ))
+        .build();
+  }
+}
