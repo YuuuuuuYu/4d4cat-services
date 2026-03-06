a122712218ad69be98fa0c842f4897f017312f31
yuuuuuuyu <yuitfhk@gmail.com>
2026-03-06 17:03:13 +0900

    chore: Add UnhandledException

--- api/src/main/java/com/services/api/presentation/GlobalExceptionHandler.java
+++ api/src/main/java/com/services/api/presentation/GlobalExceptionHandler.java
@@ -1,13 +1,8 @@
 package com.services.api.presentation;
 
 import com.services.core.dto.BaseResponse;
-import com.services.core.exception.BadGatewayException;
-import com.services.core.exception.BadRequestException;
-import com.services.core.exception.CustomException;
-import com.services.core.exception.InternalServerException;
-import com.services.core.exception.NotFoundException;
+import com.services.core.exception.*;
 import io.micrometer.core.instrument.MeterRegistry;
-import java.util.Locale;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
@@ -15,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Locale;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -25,37 +22,41 @@ public class GlobalExceptionHandler {
   private final MeterRegistry registry;
 
   private ResponseEntity<BaseResponse<Void>> createErrorResponse(
-      CustomException e, HttpStatus status) {
+      ErrorCode errorCode, HttpStatus status, Exception e) {
 
     registry
-        .counter("api.errors.total", "code", e.getErrorCode().getCode(), "status", status.name())
+        .counter("api.errors.total", "code", errorCode.getCode(), "status", status.name())
         .increment();
 
-    String message =
-        messageSource.getMessage(e.getErrorCode().getMessageKey(), null, Locale.getDefault());
-    BaseResponse<Void> response = BaseResponse.of(status, e.getErrorCode().getCode(), message);
-    log.error("Error Handled: {} - {}", e.getErrorCode().getCode(), message, (Exception) e);
+    String message = messageSource.getMessage(errorCode.getMessageKey(), null, Locale.getDefault());
+    BaseResponse<Void> response = BaseResponse.of(status, errorCode.getCode(), message);
+    log.error("Error Handled: {} - {}", errorCode.getCode(), message, e);
     return new ResponseEntity<>(response, status);
   }
 
   @ExceptionHandler(BadRequestException.class)
   public ResponseEntity<BaseResponse<Void>> handleBadRequestException(BadRequestException e) {
-    return createErrorResponse(e, HttpStatus.BAD_REQUEST);
+    return createErrorResponse(e.getErrorCode(), HttpStatus.BAD_REQUEST, e);
   }
 
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<BaseResponse<Void>> handleNotFoundException(NotFoundException e) {
-    return createErrorResponse(e, HttpStatus.NOT_FOUND);
+    return createErrorResponse(e.getErrorCode(), HttpStatus.NOT_FOUND, e);
   }
 
   @ExceptionHandler(InternalServerException.class)
   public ResponseEntity<BaseResponse<Void>> handleInternalServerException(
       InternalServerException e) {
-    return createErrorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    return createErrorResponse(e.getErrorCode(), HttpStatus.INTERNAL_SERVER_ERROR, e);
   }
 
   @ExceptionHandler(BadGatewayException.class)
   public ResponseEntity<BaseResponse<Void>> handleBadGatewayException(BadGatewayException e) {
-    return createErrorResponse(e, HttpStatus.BAD_GATEWAY);
+    return createErrorResponse(e.getErrorCode(), HttpStatus.BAD_GATEWAY, e);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<BaseResponse<Void>> handleUnhandledException(Exception e) {
+    return createErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, e);
   }
 }
