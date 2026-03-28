2ae81aa20142d4077bb34ab4cce0ee323f9eeabb
yuuuuuuyu <yuitfhk@gmail.com>
2026-03-28 18:54:11 +0900

    feat: Add Scheduler Database keep-alive

---
+++ api/src/main/java/com/services/api/common/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.services.api.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
---
+++ api/src/main/java/com/services/api/common/scheduler/DatabaseKeepAliveScheduler.java
@@ -0,0 +1,27 @@
+package com.services.api.common.scheduler;
+
+import com.services.core.aop.NotifyDiscord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseKeepAliveScheduler {
+
+  private static final Logger log = LoggerFactory.getLogger(DatabaseKeepAliveScheduler.class);
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public DatabaseKeepAliveScheduler(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Scheduled(cron = "0 0 2 * * *")
+  @NotifyDiscord(taskName = "Database Keep-alive")
+  public void keepAlive() {
+    jdbcTemplate.execute("SELECT 1");
+    log.info("Executed keep-alive dummy query to prevent database from sleeping.");
+  }
+}
