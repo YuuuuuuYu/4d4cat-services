58c19d41333c5ee0df07b76d791d8959f1e5cb5e
yuuuuuuyu <yuitfhk@gmail.com>
2026-04-10 22:33:10 +0900

    chore: Add scheduler time

--- data/src/main/java/com/services/data/scheduler/DatabaseKeepAliveScheduler.java
+++ data/src/main/java/com/services/data/scheduler/DatabaseKeepAliveScheduler.java
@@ -18,7 +18,7 @@ public class DatabaseKeepAliveScheduler {
     this.jdbcTemplate = jdbcTemplate;
   }
 
-  @Scheduled(cron = "0 0 5,17 * * *")
+  @Scheduled(cron = "0 0 1,5,9,13,17,21 * * *")
   @NotifyDiscord(taskName = "Database Keep-alive")
   public void keepAlive() {
     jdbcTemplate.execute("SELECT 1");
