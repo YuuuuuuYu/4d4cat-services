5cbf2deef47e98bba2ecb389af244de3cfb84c40
yuuuuuuyu <yuitfhk@gmail.com>
2026-06-11 14:54:17 +0900

    chore: Replace Company response

--- api/src/main/java/com/services/api/techblog/dto/TechBlogCompanyResponse.java
+++ api/src/main/java/com/services/api/techblog/dto/TechBlogCompanyResponse.java
@@ -1,9 +1,9 @@
 package com.services.api.techblog.dto;
 
-import com.services.core.common.persistence.repository.CompanyInfo;
+import com.services.core.common.dto.CompanyResponse;
 
 public record TechBlogCompanyResponse(String slug, String name) {
-  public static TechBlogCompanyResponse from(CompanyInfo info) {
-    return new TechBlogCompanyResponse(info.getSlug(), info.getName());
+  public static TechBlogCompanyResponse from(CompanyResponse info) {
+    return new TechBlogCompanyResponse(info.slug(), info.name());
   }
 }
