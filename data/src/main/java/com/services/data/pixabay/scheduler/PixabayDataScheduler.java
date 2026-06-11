fab65d03adc4e36c90548b1e844ef0fa93b19b70
yuuuuuuyu <yuitfhk@gmail.com>
2026-06-11 15:26:37 +0900

    chore: Unabled scheduler on Profile(local, test)

--- data/src/main/java/com/services/data/pixabay/scheduler/PixabayDataScheduler.java
+++ data/src/main/java/com/services/data/pixabay/scheduler/PixabayDataScheduler.java
@@ -5,11 +5,13 @@ import com.services.data.pixabay.PixabayVideoCollector;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!local & !test")
 @RequiredArgsConstructor
 @Slf4j
 public class PixabayDataScheduler {
