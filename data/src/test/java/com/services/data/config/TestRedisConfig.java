14542471d761f8ac6c0d3513db2daf61cb13d229
yuuuuuuyu <yuitfhk@gmail.com>
2026-06-20 16:22:07 +0900

    test: Fix ApplicationContext load failures in headless CI environment

--- data/src/test/java/com/services/data/config/TestRedisConfig.java
+++ data/src/test/java/com/services/data/config/TestRedisConfig.java
@@ -11,9 +11,16 @@ import java.util.Optional;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @TestConfiguration
 public class TestRedisConfig {
+
+  @Bean
+  @Primary
+  public RedisConnectionFactory redisConnectionFactory() {
+    return mock(RedisConnectionFactory.class);
+  }
 
   @Bean
   @Primary
