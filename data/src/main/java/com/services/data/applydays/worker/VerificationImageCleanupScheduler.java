5750fd641e9909f1f693eccb6ea8ecfef49e5591
yuuuuuuyu <yuitfhk@gmail.com>
2026-06-20 17:15:01 +0900

    chore: Remove unused annotation

--- data/src/main/java/com/services/data/applydays/worker/VerificationImageCleanupScheduler.java
+++ data/src/main/java/com/services/data/applydays/worker/VerificationImageCleanupScheduler.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.redis.core.RedisTemplate;
